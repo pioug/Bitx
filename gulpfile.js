25917f2a6aca32af390f6a0410fdcf836b3e3dd6
Gilles Piou <gilles.piou@gmail.com>
2016-03-23 18:40:52 +0800

    Build project in a separate directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
+const del = require('del')
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 
 gulp.task('styles', () => {
   return gulp.src('app/scss/**/*')
     .pipe(sass())
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('build/css'))
 })
+
+gulp.task('assets', () => {
+  return gulp.src([
+      '!app/scss/**/*',
+      'app/**'
+    ])
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('default', ['assets', 'styles'])
+
+gulp.task('clean', () => del('build'));
